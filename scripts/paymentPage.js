5ba86cce16929fd8523103d143016a1f8269c7a9
Rahul1905-sk <rahul1905.sk@gmail.com>
2022-12-16 04:52:14 +0530

    payment page & admin login completed

--- scripts/paymentPage.js
+++ scripts/paymentPage.js
@@ -1,8 +1,16 @@
 
-var paymentDebitButton = document.getElementById("payment-debit-button");
-var paymentWalletButton = document.getElementById("payment-wallet-button");
-var paymentUpiButton = document.getElementById("payment-upi-button");
-var paymentNetBankingButton = document.getElementById("payment-net-banking-button");
+
+var paymentDebitButton = document.getElementById("payment-debit-button-div");
+var paymentWalletButton = document.getElementById("payment-wallet-button-div");
+var paymentUpiButton = document.getElementById("payment-upi-button-div");
+var paymentNetBankingButton = document.getElementById("payment-net-banking-button-div");
+var paymentCodButton = document.getElementById("payment-cod-button-div");
+
+var paymentWallet = document.getElementById("payment-wallet-button");
+var paymentUpi = document.getElementById("payment-upi-button");
+var paymentNetBanking = document.getElementById("payment-net-banking-button");
+var paymentCod = document.getElementById("payment-cod-button");
+var paymentDebit = document.getElementById("payment-debit-button");
 
 // We have to change debit card button again on clicking second time;
 
@@ -16,12 +24,27 @@ paymentWalletButton.addEventListener("click", ()=>{
     paymentUpiButton.style.border = "none";
     paymentNetBankingButton.style.backgroundColor = "whitesmoke";
     paymentNetBankingButton.style.border = "none";
+    paymentCodButton.style.backgroundColor = "whitesmoke";
+    paymentCodButton.style.border = "none";
+
+   
+    paymentWallet.style.backgroundColor = "white";
+    paymentUpi.style.backgroundColor = "whitesmoke";
+    paymentNetBanking.style.backgroundColor = "whitesmoke";
+    paymentCod.style.backgroundColor = "whitesmoke";
+    paymentDebit.style.backgroundColor = "whitesmoke";
+   
+ 
+  
+
+
     // right div changed
     document.getElementById("payment-debit").style.display = "none";
     document.getElementById("payment-wallet").style.display = "block";
     document.getElementById("payment-debit").style.display = "none";
     document.getElementById("payment-upi").style.display = "none";
     document.getElementById("payment-net-banking").style.display = "none";
+    document.getElementById("payment-cod").style.display = "none";
 });
 
 paymentDebitButton.addEventListener("click", ()=>{
@@ -31,12 +54,25 @@ paymentDebitButton.addEventListener("click", ()=>{
     paymentNetBankingButton.style.border = "none";
     paymentWalletButton.style.backgroundColor = "whitesmoke";
     paymentWalletButton.style.borderLeft = "none";
-    paymentDebitButtonButton.style.backgroundColor = "white";
-    paymentDebitButtonButton.style.borderLeft = "5px solid #42a2a2";
+    paymentCodButton.style.backgroundColor = "whitesmoke";
+    paymentCodButton.style.borderLeft = "none";
+    paymentDebitButton.style.backgroundColor = "white";
+    paymentDebitButton.style.borderLeft = "5px solid #42a2a2";
+
+
+    paymentWallet.style.backgroundColor = "whitesmoke";
+    paymentUpi.style.backgroundColor = "whitesmoke";
+    paymentNetBanking.style.backgroundColor = "whitesmoke";
+    paymentCod.style.backgroundColor = "whitesmoke";
+    paymentDebit.style.backgroundColor = "white";
+  
+
     document.getElementById("payment-debit").style.display = "block";
     document.getElementById("payment-upi").style.display = "none";
     document.getElementById("payment-wallet").style.display = "none";
     document.getElementById("payment-net-banking").style.display = "none";
+    document.getElementById("payment-cod").style.display = "none";
+    // document.getElementById("payment-net-banking").style.display = "none";
 
 })
 
@@ -49,13 +85,25 @@ paymentDebitButton.style.backgroundColor = "whitesmoke";
     paymentWalletButton.style.borderLeft = "none";
     paymentDebitButton.style.backgroundColor = "whitesmoke";
     paymentDebitButton.style.border = "none";
+    paymentCodButton.style.backgroundColor = "whitesmoke";
+    paymentCodButton.style.border = "none";
     paymentUpiButton.style.backgroundColor = "white";
     paymentUpiButton.style.borderLeft = "5px solid #42a2a2";
+
+
+    paymentWallet.style.backgroundColor = "whitesmoke";
+    paymentUpi.style.backgroundColor = "white";
+    paymentNetBanking.style.backgroundColor = "whitesmoke";
+    paymentCod.style.backgroundColor = "whitesmoke";
+    paymentDebit.style.backgroundColor = "whitesmoke";
+
     document.getElementById("payment-upi").style.display = "block";
-    document.getElementById("payment-debit").style.display = "none";
+    // document.getElementById("payment-debit").style.display = "none";
     document.getElementById("payment-debit").style.display = "none";
     document.getElementById("payment-wallet").style.display = "none";
     document.getElementById("payment-net-banking").style.display = "none";
+    document.getElementById("payment-cod").style.display = "none";
+    // document.getElementById("payment-net-banking").style.display = "none";
 });
 
 paymentNetBankingButton.addEventListener("click", ()=>{
@@ -67,19 +115,63 @@ paymentNetBankingButton.addEventListener("click", ()=>{
     paymentWalletButton.style.borderLeft = "none";
     paymentDebitButton.style.backgroundColor = "whitesmoke";
     paymentDebitButton.style.border = "none";
+    paymentCodButton.style.backgroundColor = "whitesmoke";
+    paymentCodButton.style.border = "none";
     paymentNetBankingButton.style.backgroundColor = "white";
     paymentNetBankingButton.style.borderLeft = "5px solid #42a2a2";
+
+
+    paymentWallet.style.backgroundColor = "whitesmoke";
+    paymentUpi.style.backgroundColor = "whitesmoke";
+    paymentNetBanking.style.backgroundColor = "white";
+    paymentCod.style.backgroundColor = "whitesmoke";
+    paymentDebit.style.backgroundColor = "whitesmoke";
+
     document.getElementById("payment-net-banking").style.display = "block";
     document.getElementById("payment-debit").style.display = "none";
     document.getElementById("payment-upi").style.display = "none";
     document.getElementById("payment-wallet").style.display = "none";
+    document.getElementById("payment-cod").style.display = "none";
+    // document.getElementById("payment-wallet").style.display = "none";
 });
+
+  paymentCodButton.addEventListener("click", ()=>{
+  paymentDebitButton.style.backgroundColor = "whitesmoke";
+  paymentDebitButton.style.border = "none";
+  paymentUpiButton.style.backgroundColor = "whitesmoke";
+  paymentUpiButton.style.border = "none";
+  paymentWalletButton.style.backgroundColor = "whitesmoke";
+  paymentWalletButton.style.borderLeft = "none";
+  paymentDebitButton.style.backgroundColor = "whitesmoke";
+  paymentDebitButton.style.border = "none";
+  paymentNetBankingButton.style.backgroundColor = "whitesmoke";
+  paymentNetBankingButton.style.borderLeft = "none";
+  paymentCodButton.style.backgroundColor = "white";
+  paymentCodButton.style.borderLeft = "5px solid #42a2a2";
+
+  
+  paymentWallet.style.backgroundColor = "whitesmoke";
+  paymentUpi.style.backgroundColor = "whitesmoke";
+  paymentNetBanking.style.backgroundColor = "whitesmoke";
+  paymentCod.style.backgroundColor = "white";
+  paymentDebit.style.backgroundColor = "whitesmoke";
+  
+  document.getElementById("payment-net-banking").style.display = "none";
+  document.getElementById("payment-debit").style.display = "none";
+  document.getElementById("payment-upi").style.display = "none";
+  document.getElementById("payment-wallet").style.display = "none";
+  // document.getElementById("payment-cod-button").style.display = "block";
+  document.getElementById("payment-cod").style.display = "block";
+
+});
+
+
 document.getElementById("card-pay-now-button").addEventListener("click" , function(){
 
   var person = prompt("Please enter your OTP", "");
   if(person == "1234"){
 alert("We are processing your payment !");
-window.location.href = "./paymentsuccesspage.html";
+window.location.href = "./orderPlaced.html";
 }
   else{
 alert("Wrong OTP, Try Again");
@@ -87,3 +179,35 @@ alert("Wrong OTP, Try Again");
 });
 
 
+
+function paymentP() {
+ 
+    paymentUpiButton.style.backgroundColor = "whitesmoke";
+    paymentUpiButton.style.border = "none";
+    paymentNetBankingButton.style.backgroundColor = "whitesmoke";
+    paymentNetBankingButton.style.border = "none";
+    paymentWalletButton.style.backgroundColor = "whitesmoke";
+    paymentWalletButton.style.borderLeft = "none";
+    paymentCodButton.style.backgroundColor = "whitesmoke";
+    paymentCodButton.style.borderLeft = "none";
+    paymentDebitButton.style.backgroundColor = "white";
+    paymentDebitButton.style.borderLeft = "5px solid #42a2a2";
+
+
+    paymentWallet.style.backgroundColor = "whitesmoke";
+    paymentUpi.style.backgroundColor = "whitesmoke";
+    paymentNetBanking.style.backgroundColor = "whitesmoke";
+    paymentCod.style.backgroundColor = "whitesmoke";
+    paymentDebit.style.backgroundColor = "white";
+  
+
+    document.getElementById("payment-debit").style.display = "block";
+    document.getElementById("payment-upi").style.display = "none";
+    document.getElementById("payment-wallet").style.display = "none";
+    document.getElementById("payment-net-banking").style.display = "none";
+    document.getElementById("payment-cod").style.display = "none";
+    // document.getElementById("payment-net-banking").style.display = "none";
+
+}
+
+paymentP()
