f25cb5e6b9b47a3be8ea854303c8af518386c7ea
Rahul1905-sk <rahul1905.sk@gmail.com>
2022-12-17 17:33:17 +0530

    loader added

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ function footerSection() {
 
         <!-- customer section start -->
 
-        <h1>BeCooL</h1>
+        <h1 <a href=".index.html">BEcool</h1></a>
 
         <div class="customer-section-grid">
 
--- scripts/orderPlaced.js
+++ scripts/orderPlaced.js
@@ -2,6 +2,9 @@
 
 import {navbar} from "../components/navbar.js";
 import { footerSection } from "../components/footer.js";
+import {loader} from "../components/loader.js"
+loader()
+
 
 let navbar1 = document.querySelector("#navbar");
 navbar1.innerHTML = navbar();
@@ -9,3 +12,7 @@ navbar1.innerHTML = navbar();
 
 let footer1=document.getElementById("footer");
 footer1.innerHTML = footerSection();
+
+
+
+
